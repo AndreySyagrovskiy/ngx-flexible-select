467c6a343680e2de324f58219aa7156b14b8d5fa
Andrii Siahrovskyi <asiah@thrills.softservecom.com>
2019-05-29 16:39:20 +0300

    addresses and button click were added to demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngx-flexible-select-demo';
+  address = null;
+  addressList = [
+    {
+      id: '1',
+      country: 'USA',
+      city: 'New York',
+      address: 'Dekalb Avenue',
+      house: '101',
+      flat: '12'
+    },
+    {
+      id: '2',
+      country: 'France',
+      city: 'Paris',
+      address: 'des Champs-Élysées',
+      house: '21',
+      flat: '1'
+    },
+    {
+      id: '3',
+      country: 'Great Britain',
+      city: 'London',
+      address: 'Arthur Street',
+      house: '91',
+      flat: '66'
+    }
+  ];
+
+  helloWorldAlert(name: string) {
+    alert(`Hello, ${name}!`);
+  }
+
+  trackBy(index, item) {
+    return item.id;
+  }
 }
